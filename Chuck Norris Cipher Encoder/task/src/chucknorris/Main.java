1ceb40ce087b6ec312fdf4316ca7def9b5e7a6a1
Dexter <gusevsergey85@gmail.com>
2023-01-11 12:58:54 +0300

    Second stage

--- Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
+++ Chuck Norris Cipher Encoder/task/src/chucknorris/Main.java
@@ -1,5 +1,7 @@
 package chucknorris;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,9 +10,26 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         System.out.println("Input string: ");
         String str = scan.nextLine();
+        System.out.println("The result:");
         for (int i = 0; i < str.length(); i++) {
-            System.out.print(str.charAt(i) + " ");
+            System.out.printf("%c = %07d\n", str.charAt(i), Integer.parseInt(getBin(str.charAt(i))));
 
         }
+    }
+
+    public static String getBin(char charecter) {
+        int number = charecter;
+        ArrayList<Integer> binary = new ArrayList<>();
+        String result = "";
+        while (number != 0) {
+            binary.add(number % 2);
+            number /= 2;
+        }
+        for (int i = binary.size() - 1; i >= 0 ; i--) {
+            result += binary.get(i).toString();
+        }
+
+
+        return result;
     }
 }
